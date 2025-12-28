69cf1f5c1e892503b8c024254133ae285c7fab4e
poorneshvishnole <purneshvishnole@gmail.com>
2025-12-28 23:19:23 +0530

    first commit

---
+++ src/01_inference.ts
@@ -0,0 +1,13 @@
+let count = 0;
+let nam = "Poorneshsvavmsvavs";
+let maybe: string | number;
+
+let x = Math.random() > 0.6 ? "ghdsgdhgsd" : 67;
+
+function addTwoNumbers(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(addTwoNumbers(0, 7));
+
+//Inference - the compiler's ability to automatically figure out a variable's type from its assigned value
